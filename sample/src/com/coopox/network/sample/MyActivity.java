7b349292e566ba8c321528a5b9e159b1e5caeb90
lokii <coredump8@gmail.com>
2015-02-04 01:44:52 +0800

    Add a sample app

--- sample/src/com/coopox/network/sample/MyActivity.java
+++ sample/src/com/coopox/network/sample/MyActivity.java
@@ -1,9 +1,42 @@
 package com.coopox.network.sample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import com.coopox.network.http.DownloadService;
 
-public class MyActivity extends Activity {
+import java.io.File;
+
+public class MyActivity extends Activity implements View.OnClickListener {
+
+    private ProgressBar mProgressBar;
+    private EditText mEditText;
+    private Handler mResponseHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            Log.d("handleMessage", "Download Message: " + msg.what);
+            switch (msg.what) {
+                case DownloadService.MSG_PROGRESS_UPDATE:
+                    Log.d("handleMessage", "Downloaded " + msg.arg2 + "%");
+                    mProgressBar.setProgress(msg.arg2);
+                    break;
+                case DownloadService.MSG_CANCELLED:
+                    mProgressBar.setProgress(0);
+                    break;
+            }
+        }
+    };
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +44,38 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress);
+        mEditText = (EditText) findViewById(R.id.url_input);
+        Button start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(this);
+        Button stop = (Button) findViewById(R.id.stop);
+        stop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String url = mEditText.getText().toString();
+        switch (v.getId()) {
+            case R.id.start:
+                if (URLUtil.isHttpUrl(url)) {
+                    Intent intent = new Intent(this, DownloadService.class);
+                    intent.putExtra(DownloadService.EXTRA_URL, url);
+                    File dir = getExternalCacheDir();
+                    File output = new File(dir, "test.tmp");
+                    intent.putExtra(DownloadService.EXTRA_OUTPUT_PATH, output.getPath());
+                    intent.putExtra(DownloadService.EXTRA_MESSENGER, new Messenger(mResponseHandler));
+                    startService(intent);
+                }
+                break;
+            case R.id.stop:
+                if (URLUtil.isHttpUrl(url)) {
+                    Intent intent = new Intent(this, DownloadService.class);
+                    intent.putExtra(DownloadService.EXTRA_URL, url);
+                    intent.putExtra(DownloadService.EXTRA_STOP, true);
+                    intent.putExtra(DownloadService.EXTRA_MESSENGER, new Messenger(mResponseHandler));
+                    startService(intent);
+                }
+                break;
+        }
     }
 }
--- src/com/coopox/network/http/DownloadService.java
+++ src/com/coopox/network/http/DownloadService.java
@@ -11,6 +11,8 @@ import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Hashtable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,30 +20,36 @@ import java.util.Hashtable;
  * Date: 15/2/1
  */
 public class DownloadService extends Service {
+    private static final String TAG = "DownloadService";
 
     private static final int CONNECT_TIMEOUT = 5000;
     private static final int READ_TIMEOUT = 5000;
 
     public static final int MSG_STARTED = 0x0;
     public static final int MSG_PROGRESS_UPDATE = 0x1;
-    public static final int MSG_CANCELED = 0x2;
-    public static final int MSG_COMPLETED = 0x3;
+    public static final int MSG_CANCELLED = 0x2;
+    public static final int MSG_SUCCESS = 0x3;
     public static final int MSG_FAILED = 0x4;
 
     public static final String ACTION_STATED = "DownloadService.Started";
     public static final String ACTION_PROGRESS_UPDATE = "DownloadService.Update";
-    public static final String ACTION_CANCELED = "DownloadService.Canceled";
-    public static final String ACTION_COMPLETED = "DownloadService.Completed";
+    public static final String ACTION_CANCELLED = "DownloadService.Cancelled";
+    public static final String ACTION_SUCCESS = "DownloadService.Success";
     public static final String ACTION_FAILED = "DownloadService.Failed";
 
     public static final String[] MSG_TO_ACTION = {
             ACTION_STATED,
             ACTION_PROGRESS_UPDATE,
-            ACTION_CANCELED,
-            ACTION_COMPLETED,
+            ACTION_CANCELLED,
+            ACTION_SUCCESS,
             ACTION_FAILED
     };
-    private static final String TAG = "DownloadService";
+
+    public static final String EXTRA_URL = "URL";
+    public static final String EXTRA_OUTPUT_PATH = "OutputPath";
+    public static final String EXTRA_MESSENGER = "Messenger";
+    public static final String EXTRA_STOP = "Stop";
+    private static final int MAX_THREADS = 3;
 
     class DownloadTask {
         public DownloadTask(String url, String output, Messenger messenger) {
@@ -55,13 +63,21 @@ public class DownloadService extends Service {
         public final Messenger mMessenger;
         public HttpURLConnection mConnection;
         public DownloadFileTask mAsyncTask;
+        public DownloadFileRunnable mRunnable;
     }
 
     private Hashtable<String, DownloadTask> mTasks = new Hashtable<String, DownloadTask>();
+    private ExecutorService mThreadPool = Executors.newFixedThreadPool(MAX_THREADS);
 
-    @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 
     @Override
@@ -72,17 +88,17 @@ public class DownloadService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (null != intent) {
-            String url = intent.getStringExtra("URL");
-            String outputPath = intent.getStringExtra("OutputPath");
-            Messenger messenger = intent.getParcelableExtra("Messenger");
-            boolean needStop = intent.getBooleanExtra("Stop", false);
+            String url = intent.getStringExtra(EXTRA_URL);
+            String outputPath = intent.getStringExtra(EXTRA_OUTPUT_PATH);
+            Messenger messenger = intent.getParcelableExtra(EXTRA_MESSENGER);
+            boolean needStop = intent.getBooleanExtra(EXTRA_STOP, false);
 
-            if (null != url && null != outputPath) {
+            if (null != url) {
                 if (needStop) {
-                    if (mTasks.contains(url)) {
+                    if (mTasks.containsKey(url)) {
                         stopDownload(mTasks.get(url));
                     }
-                } else {
+                } else if (null != outputPath) {
                     DownloadTask task = mTasks.get(url);
                     if (null == task) {
                         task = new DownloadTask(url, outputPath, messenger);
@@ -93,19 +109,211 @@ public class DownloadService extends Service {
                     }
                 }
             }
+
+            if (mTasks.isEmpty()) {
+                stopSelf();
+            }
         }
-        return super.onStartCommand(intent, flags, startId);
+        return START_NOT_STICKY;
     }
 
     private void startDownload(DownloadTask task) {
         if (null != task) {
-            task.mAsyncTask = new DownloadFileTask(this, task);
-            task.mAsyncTask.execute();
+            task.mRunnable = new DownloadFileRunnable(this, task);
+            mThreadPool.execute(task.mRunnable);
         }
     }
 
     private void stopDownload(DownloadTask task) {
-        task.mAsyncTask.cancel(true);
+        if (null != task && null != task.mRunnable) {
+            task.mRunnable.stop();
+        }
+    }
+
+    static class DownloadFileRunnable implements Runnable {
+
+        private static final int CACHE_BLOCK_SIZE = 4096;
+        private final DownloadTask mTask;
+        private final WeakReference<Context> mContextRef;
+        private volatile boolean mStoped;
+
+        public DownloadFileRunnable(Context context, DownloadTask task) {
+            mContextRef = new WeakReference<Context>(context);
+            mTask = task;
+        }
+
+        public void stop() {
+            mStoped = true;
+        }
+
+        @Override
+        public void run() {
+            if (mStoped) {
+                onDownloadCancelled();
+                return;
+            }
+
+            DownloadTask task = mTask;
+            FileOutputStream outputStream = null;
+            InputStream inputStream = null;
+            Exception exception = null;
+
+            onStartDownload();
+            try {
+                URL url = new URL(task.mUrl);
+                task.mConnection = (HttpURLConnection) url.openConnection();
+                task.mConnection.setConnectTimeout(CONNECT_TIMEOUT);
+                task.mConnection.setReadTimeout(READ_TIMEOUT);
+                task.mConnection.setInstanceFollowRedirects(true);
+
+                if (task.mConnection.getResponseCode() == 200) {
+                    int totalSize = task.mConnection.getContentLength();
+
+                    File outputFile = new File(task.mOutput);
+                    outputStream = new FileOutputStream(outputFile);
+
+                    inputStream = task.mConnection.getInputStream();
+
+                    byte[] buffer = new byte[CACHE_BLOCK_SIZE];
+
+                    int len;
+                    int downloadSize = 0;
+                    int lastPercent = 0;
+                    while (!mStoped &&
+                            (len = inputStream.read(buffer, 0, buffer.length)) != -1) {
+                        outputStream.write(buffer, 0, len);
+                        downloadSize += len;
+
+                        if (totalSize > 0) {
+                            int percent = (int) ((downloadSize / (float) totalSize) * 100);
+                            // Update progress when percent changed.
+                            if (percent != lastPercent) {
+                                lastPercent = percent;
+                                sendMessageToClient(MSG_PROGRESS_UPDATE, 0, percent, mTask.mUrl, null);
+                            }
+                        } else {
+                            sendMessageToClient(MSG_PROGRESS_UPDATE, 0, downloadSize, mTask.mUrl, null);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                exception = e;
+            } finally {
+                if (null != outputStream) {
+                    Log.i(TAG, String.format("Close output Stream for %s", task.mUrl));
+                    try {
+                        outputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        exception = e;
+                    }
+                }
+
+                clearTask();
+
+                if (mStoped) {
+                    onDownloadCancelled();
+                } else {
+                    onDownloadOver(exception);
+                }
+
+                if (null != inputStream) {
+                    Log.i(TAG, String.format("Close input Stream for %s", task.mUrl));
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                Log.i(TAG, String.format("Close connection for %s", task.mUrl));
+                if (null != task.mConnection) {
+                    task.mConnection.disconnect();
+                    task.mConnection = null;
+                }
+
+                checkWhetherStopService();
+            }
+        }
+
+        private void onStartDownload() {
+            sendMessageToClient(MSG_STARTED, 0, 0, mTask.mUrl, null);
+        }
+
+        private void onDownloadCancelled() {
+            Log.d(TAG, String.format("Task %s was cancelled", mTask.mUrl));
+            sendMessageToClient(MSG_CANCELLED, 0, 0, mTask.mUrl, null);
+        }
+
+        private void onDownloadOver(Exception e) {
+            Log.d(TAG, String.format("Task %s was Over", mTask.mUrl));
+            if (null == e) {
+                sendMessageToClient(MSG_SUCCESS, 0, 0, mTask.mUrl, null);
+            } else {
+                Bundle data = new Bundle();
+                data.putString("error", e.toString());
+                sendMessageToClient(MSG_FAILED, 0, 0, mTask.mUrl, data);
+            }
+        }
+
+        private DownloadTask clearTask() {
+            // Download finish, remove the task from queue.
+            Context context = mContextRef.get();
+            if (context instanceof DownloadService) {
+                DownloadService service = (DownloadService)context;
+                return service.mTasks.remove(mTask.mUrl);
+            }
+            return null;
+        }
+
+        private void checkWhetherStopService() {
+            Context context = mContextRef.get();
+            if (context instanceof DownloadService) {
+                DownloadService service = (DownloadService)context;
+                if (service.mTasks.isEmpty()) {
+                    service.stopSelf();
+                }
+            }
+        }
+
+        private void sendMessageToClient(int what, int arg1, int arg2, Serializable obj, Bundle data) {
+            Messenger messenger = mTask.mMessenger;
+            if (null != messenger) {
+                Message msg = Message.obtain();
+                msg.what = what;
+                msg.arg1 = arg1;
+                msg.arg2 = arg2;
+                msg.obj = obj;
+                if (null != data) {
+                    msg.setData(data);
+                }
+
+                try {
+                    messenger.send(msg);
+                    return;
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+            sendBroadcastToClient(what, arg1, arg2, obj, data);
+        }
+
+        private void sendBroadcastToClient(int what, int arg1, int arg2, Serializable obj, Bundle data) {
+            Context context = mContextRef.get();
+            if (null != context) {
+                Intent intent = new Intent(MSG_TO_ACTION[what]);
+                intent.putExtra("arg1", arg1);
+                intent.putExtra("arg2", arg2);
+                if (null != obj) {
+                    intent.putExtra("obj", obj);
+                }
+                if (null != data) {
+                    intent.putExtra("data", data);
+                }
+                context.sendBroadcast(intent);
+            }
+        }
     }
 
     static class DownloadFileTask extends AsyncTask<Object, Integer, Boolean> {
@@ -143,12 +351,18 @@ public class DownloadService extends Service {
 
                     int len;
                     int downloadSize = 0;
+                    int lastPercent = 0;
                     while ((len = inputStream.read(buffer, 0, buffer.length)) != -1) {
                         outputStream.write(buffer, 0, len);
                         downloadSize += len;
 
                         if (totalSize > 0) {
-                            publishProgress((int) ((downloadSize / (float) totalSize) * 100));
+                            int percent = (int) ((downloadSize / (float) totalSize) * 100);
+                            // Update progress when percent changed.
+                            if (percent != lastPercent) {
+                                lastPercent = percent;
+                                publishProgress(percent);
+                            }
                         } else {
                             publishProgress(downloadSize);
                         }
@@ -186,14 +400,16 @@ public class DownloadService extends Service {
 
         @Override
         protected void onCancelled() {
-            sendMessageToClient(MSG_CANCELED, 0, 0, mTask.mUrl);
+            Log.d(TAG, String.format("Task %s was cancelled", mTask.mUrl));
+            sendMessageToClient(MSG_CANCELLED, 0, 0, mTask.mUrl);
             removeTask();
         }
 
         @Override
         protected void onPostExecute(Boolean result) {
+            Log.d(TAG, String.format("Task %s was completed", mTask.mUrl));
             if (result) {
-                sendMessageToClient(MSG_COMPLETED, 0, 0, mTask.mUrl);
+                sendMessageToClient(MSG_SUCCESS, 0, 0, mTask.mUrl);
             } else {
                 sendMessageToClient(MSG_FAILED, 0, 0, mTask.mUrl);
             }
@@ -206,6 +422,9 @@ public class DownloadService extends Service {
             if (context instanceof DownloadService) {
                 DownloadService service = (DownloadService)context;
                 service.mTasks.remove(mTask.mUrl);
+                if (service.mTasks.isEmpty()) {
+                    service.stopSelf();
+                }
             }
         }
 
